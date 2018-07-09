dc318390fd8615da35fb48d032389774c4fe06f1
Eric Golde <eric@golde.org>
2018-07-08 21:56:03 -0700

    mostly fixed color picker

--- app/emilylights/src/components/color-picker/color-picker.ts
+++ app/emilylights/src/components/color-picker/color-picker.ts
@@ -1,15 +1,15 @@
-import {Component, Output, Input, ViewChild,EventEmitter} from '@angular/core';
+import { Component, Output, Input, ViewChild, EventEmitter } from '@angular/core';
 
 const POUCH = [
   {
-    START : "mousedown",
-    MOVE : "mousemove",
-    STOP : "mouseup"
+    START: "mousedown",
+    MOVE: "mousemove",
+    STOP: "mouseup"
   },
   {
-    START : "touchstart",
-    MOVE : "touchmove",
-    STOP : "touchend"
+    START: "touchstart",
+    MOVE: "touchmove",
+    STOP: "touchend"
   }
 ];
 
@@ -20,7 +20,7 @@ const POUCH = [
 })
 export class ColorPicker {
 
-  @Input() hexColor : string;
+  @Input() hexColor: string;
 
   @Output() colorChanged = new EventEmitter<String>();
 
@@ -34,35 +34,35 @@ export class ColorPicker {
   // this is the color chooser
   @ViewChild("chooser") chooser;
 
-  ctxPalette : CanvasRenderingContext2D;
+  ctxPalette: CanvasRenderingContext2D;
 
-  requestAnimationFrameID : number;
+  requestAnimationFrameID: number;
 
-  color : string;
+  color: string;
 
-  colorFromChooser : string;
+  colorFromChooser: string;
 
-  paletteX : number;
+  paletteX: number;
 
-  paletteY : number;
+  paletteY: number;
 
-  chooserX : number;
+  chooserX: number;
 
   public ngOnInit() {
-    if(this.hexColor){
+    if (this.hexColor) {
       this.colorFromChooser = this.hexColor;
-    } else{
+    } else {
       this.colorFromChooser = "#0000FF";
     }
     this.init();
   }
 
-  init(){
+  init() {
     this.initChooser();
     this.initPalette();
   }
 
-  drawSelector(ctx : CanvasRenderingContext2D, x : number, y : number ){
+  drawSelector(ctx: CanvasRenderingContext2D, x: number, y: number) {
     this.drawPalette(this.colorFromChooser);
     ctx.beginPath();
     ctx.arc(x, y, 10 * this.getPixelRatio(ctx), 0, 2 * Math.PI, false);
@@ -73,7 +73,7 @@ export class ColorPicker {
     ctx.stroke();
   }
 
-  drawChooserSelector(ctx : CanvasRenderingContext2D, x : number ){
+  drawChooserSelector(ctx: CanvasRenderingContext2D, x: number) {
     this.drawPalette(this.colorFromChooser);
     ctx.beginPath();
     ctx.arc(x, ctx.canvas.height / 2, ctx.canvas.height / 2, 0, 2 * Math.PI, false);
@@ -84,7 +84,7 @@ export class ColorPicker {
     ctx.stroke();
   }
 
-  initPalette(){
+  initPalette() {
     let canvasPalette = this.palette.nativeElement;
     this.ctxPalette = canvasPalette.getContext("2d");
 
@@ -94,10 +94,10 @@ export class ColorPicker {
 
     console.log(pixelRatio);
 
-    var width = currentWidth * 90/100;
+    var width = currentWidth * 90 / 100;
     var height = width * 0.5;
 
-    this.ctxPalette.canvas.width  = width * pixelRatio;
+    this.ctxPalette.canvas.width = width * pixelRatio;
     this.ctxPalette.canvas.height = height * pixelRatio;
 
     this.ctxPalette.canvas.style.width = width + "px";
@@ -126,15 +126,17 @@ export class ColorPicker {
     })
   }
 
-  drawPalette(endColor : string){
+  drawPalette(endColor: string) {
+
+    if (endColor == null) { return; }
 
     this.ctxPalette.clearRect(0, 0, this.ctxPalette.canvas.width, this.ctxPalette.canvas.height);
 
     var gradient = this.ctxPalette.createLinearGradient(0, 0, this.ctxPalette.canvas.width, 0);
 
     // Create color gradient
-    gradient.addColorStop(0,    "#FFFFFF");
-    gradient.addColorStop(1,    endColor);
+    gradient.addColorStop(0, "#FFFFFF");
+    gradient.addColorStop(1, endColor);
 
     // Apply gradient to canvas
     this.ctxPalette.fillStyle = gradient;
@@ -142,17 +144,17 @@ export class ColorPicker {
 
     // Create semi transparent gradient (white -> trans. -> black)
     gradient = this.ctxPalette.createLinearGradient(0, 0, 0, this.ctxPalette.canvas.height);
-    gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
+    gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
     gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
     gradient.addColorStop(0.5, "rgba(0,     0,   0, 0)");
-    gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
+    gradient.addColorStop(1, "rgba(0,     0,   0, 1)");
 
     // Apply gradient to canvas
     this.ctxPalette.fillStyle = gradient;
     this.ctxPalette.fillRect(0, 0, this.ctxPalette.canvas.width, this.ctxPalette.canvas.height);
   }
 
-  initChooser(){
+  initChooser() {
     let canvasChooser = this.chooser.nativeElement;
     var ctx = canvasChooser.getContext("2d");
 
@@ -160,10 +162,10 @@ export class ColorPicker {
 
     var pixelRatio = this.getPixelRatio(ctx);
 
-    var width = currentWidth * 90/100;
+    var width = currentWidth * 90 / 100;
     var height = width * 0.05;
 
-    ctx.canvas.width  = width * pixelRatio;
+    ctx.canvas.width = width * pixelRatio;
     ctx.canvas.height = height * pixelRatio;
 
     ctx.canvas.style.width = width + "px";
@@ -171,9 +173,11 @@ export class ColorPicker {
 
     this.drawChooser(ctx);
 
+    //EVENTS For gradient bottom
     var eventChangeColorChooser = (event) => {
       this.updateColorChooser(event, canvasChooser, ctx);
-      this.drawSelector(this.ctxPalette, this.ctxPalette.canvas.width, this.ctxPalette.canvas.height / 2);
+      this.drawSelector(this.ctxPalette, this.paletteX, this.paletteY);
+      //console.log("Move");
     };
 
     POUCH.forEach(pouch => {
@@ -181,7 +185,8 @@ export class ColorPicker {
         this.drawChooser(ctx);
         canvasChooser.addEventListener(pouch.MOVE, eventChangeColorChooser);
         this.updateColorChooser(event, canvasChooser, ctx);
-        this.drawSelector(this.ctxPalette, this.ctxPalette.canvas.width, this.ctxPalette.canvas.height / 2);
+        this.drawSelector(this.ctxPalette, this.paletteX, this.paletteY);
+        //console.log("Start");
       });
 
       canvasChooser.addEventListener(pouch.STOP, (event) => {
@@ -189,71 +194,83 @@ export class ColorPicker {
         this.updateColorChooser(event, canvasChooser, ctx);
         this.drawChooser(ctx);
         this.drawChooserSelector(ctx, this.chooserX);
-        this.drawSelector(this.ctxPalette, this.ctxPalette.canvas.width, this.ctxPalette.canvas.height / 2);
+        this.drawSelector(this.ctxPalette, this.paletteX, this.paletteY);
+        //console.log("Stop");
       });
     });
   }
 
-  drawChooser(ctx : CanvasRenderingContext2D){
+  drawChooser(ctx: CanvasRenderingContext2D) {
 
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
     var gradient = ctx.createLinearGradient(0, 0, ctx.canvas.width, 0);
 
     // Create color gradient
-    gradient.addColorStop(0,    "rgb(255,   0,   0)");
+    gradient.addColorStop(0, "rgb(255,   0,   0)");
     gradient.addColorStop(0.15, "rgb(255,   0, 255)");
     gradient.addColorStop(0.33, "rgb(0,     0, 255)");
     gradient.addColorStop(0.49, "rgb(0,   255, 255)");
     gradient.addColorStop(0.67, "rgb(0,   255,   0)");
     gradient.addColorStop(0.84, "rgb(255, 255,   0)");
-    gradient.addColorStop(1,    "rgb(255,   0,   0)");
+    gradient.addColorStop(1, "rgb(255,   0,   0)");
 
     // Apply gradient to canvas
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
   }
 
-  getPixelRatio(ctx){
+  getPixelRatio(ctx) {
     var dpr = window.devicePixelRatio || 1;
 
     var bsr = ctx.webkitBackingStorePixelRatio ||
-    ctx.mozBackingStorePixelRatio ||
-    ctx.msBackingStorePixelRatio ||
-    ctx.oBackingStorePixelRatio ||
-    ctx.backingStorePixelRatio || 1;
+      ctx.mozBackingStorePixelRatio ||
+      ctx.msBackingStorePixelRatio ||
+      ctx.oBackingStorePixelRatio ||
+      ctx.backingStorePixelRatio || 1;
 
     return dpr / bsr;
   }
 
-  updateColorChooser(event, canvas, context){
-    this.color = this.colorFromChooser = this.getColor(event, canvas, context, true);
+  updateColorChooser(event, canvas, context) {
+    var newColor = this.getColor(event, canvas, context, true);;
+    if (newColor == null) { return; }
+    this.color = this.colorFromChooser = newColor;
     this.colorChanged.emit(this.color);
     this.drawPalette(this.color);
   }
 
-  updateColor(event, canvas, context){
-    this.color = this.getColor(event, canvas, context, false);
+  updateColor(event, canvas, context) {
+    var newColor = this.getColor(event, canvas, context, false);
+    if (newColor == null) { return; }
+    this.color = newColor;
     this.colorChanged.emit(this.color);
   }
 
-  getColor(event, canvas, context, fromChooser : boolean) : string {
+  getColor(event, canvas, context, fromChooser: boolean): string {
 
     var bounding = canvas.getBoundingClientRect(),
-    touchX = event.pageX || event.changedTouches[0].pageX || event.changedTouches[0].screenX,
-    touchY = event.pageY || event.changedTouches[0].pageY || event.changedTouches[0].screenX;
+      touchX = event.pageX || event.changedTouches[0].pageX || event.changedTouches[0].screenX,
+      touchY = event.pageY || event.changedTouches[0].pageY || event.changedTouches[0].screenX;
+
+    if (touchX >= bounding.right || touchX < bounding.left || touchY >= bounding.bottom || touchY < bounding.top) {
+      return null;
+    }
 
     var x = (touchX - bounding.left) * this.getPixelRatio(context);
     var y = (touchY - bounding.top) * this.getPixelRatio(context);
 
-    if(fromChooser){
+
+
+    if (fromChooser) {
       this.chooserX = x;
-    } else{
+    } else {
+
       this.paletteX = x;
       this.paletteY = y;
     }
 
-    var imageData = context.getImageData(x,  y, 1, 1);
+    var imageData = context.getImageData(x, y, 1, 1);
     var red = imageData.data[0];
     var green = imageData.data[1];
     var blue = imageData.data[2];
@@ -261,9 +278,9 @@ export class ColorPicker {
   }
 
   toHex(n) {
-    n = parseInt(n,10);
+    n = parseInt(n, 10);
     if (isNaN(n)) return "00";
-    n = Math.max(0,Math.min(n,255));
-    return "0123456789ABCDEF".charAt((n-n%16)/16)  + "0123456789ABCDEF".charAt(n%16);
+    n = Math.max(0, Math.min(n, 255));
+    return "0123456789ABCDEF".charAt((n - n % 16) / 16) + "0123456789ABCDEF".charAt(n % 16);
   }
 }
